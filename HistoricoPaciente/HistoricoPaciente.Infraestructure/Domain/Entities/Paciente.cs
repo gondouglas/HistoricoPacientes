258bccb05aa9c7927f3c369ca22328a862d18755
Douglas Gonçalves <gondouglas43@gmail.com>
2018-09-26 23:08:50 -0300

    Remove project

---
+++ HistoricoPaciente/HistoricoPaciente.Infraestructure/Domain/Entities/Paciente.cs
@@ -0,0 +1,14 @@
+﻿using HistoricoPaciente.Infra.Domain.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HistoricoPaciente.Infra.Domain.Entities
+{
+    public class Paciente : BaseEntity
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public char sexo { get; set; }
+    }
+}
