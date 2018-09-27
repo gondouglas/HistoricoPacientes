dfb0f99252e53004cda9b6f6de13df930ac1e50d
Douglas Gonçalves <gondouglas43@gmail.com>
2018-09-26 22:55:04 -0300

    First Commit

---
+++ HistoricoPaciente/Infraestrutura/Domain/Entities/Historico.cs
@@ -0,0 +1,14 @@
+﻿using HistoricoPaciente.Infra.Domain.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HistoricoPaciente.Infra.Domain.Entities
+{
+    public class Historico : BaseEntity
+    {
+        public Paciente Paciente { get; set; }
+        public string Descricao { get; set; }
+        public DateTime Data { get; set; }
+    }
+}
